49eae04a760f273c816fd535336091cc4c20fbc7
Keano Bellmore <keanobellmore@Interapts-MacBook-Pro-3.local>
2018-04-26 14:53:02 -0400

    proj start

---
+++ weather app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather app
+//
+//  Created by Keano Bellmore on 4/26/18.
+//  Copyright © 2018 Keano Bellmore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
